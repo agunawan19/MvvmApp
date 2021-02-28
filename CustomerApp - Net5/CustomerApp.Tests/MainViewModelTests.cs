aa0a12b8ba43bb743d19c8d603e46212b8b77063
Andri Gunawan <agunawan@programmer.net>
2021-02-28 00:38:32 -0800

    Replace FakeItEasy with Moq

--- CustomerApp - Net5/CustomerApp.Tests/MainViewModelTests.cs
+++ CustomerApp - Net5/CustomerApp.Tests/MainViewModelTests.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Data;
-using CustomerApp.ViewModel;
-using CustomerLib;
-using FakeItEasy;
+
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Times = Moq.Times;
+
+using CustomerApp.ViewModel;
+using CustomerLib;
 
 namespace CustomerApp.Tests
 {
@@ -24,43 +27,52 @@ namespace CustomerApp.Tests
         [TestMethod]
         public void Constructor_Customers_ShouldHaveValue()
         {
-            var repository = A.Fake<ICustomerRepository>();
+            var repository = Mock.Of<ICustomerRepository>();
             var customers = new List<Customer>();
-            A.CallTo(() => repository.Customers).Returns(customers);
+            
             var vm =  new MainViewModel(repository);
+            
             vm.Customers.Should().BeEquivalentTo(customers);
         }
         
         [TestMethod]
         public void Constructor_SelectedCustomer_ShouldBeNull()
         {
-            var repository = A.Fake<ICustomerRepository>();
+            var repository = Mock.Of<ICustomerRepository>();
+           
             var vm = new MainViewModel(repository);
+            
             vm.SelectedCustomer.Should().BeNull();
         }
         
         [TestMethod]
         public void AddCommand_ShouldAddInRepository()
         {
-            var repository = A.Fake<ICustomerRepository>();
-            var vm = new MainViewModel(repository);
+            var repositoryMock = new Mock<ICustomerRepository>();
+            repositoryMock.Setup(repository => repository.Add(It.IsAny<Customer>())).Returns(true);
+            var vm = new MainViewModel(repositoryMock.Object);
+            
             vm.AddCommand.Execute(null);
-            A.CallTo(() => repository.Add(A<Customer>._)).MustHaveHappened();
+            
+            repositoryMock.Verify(repository => repository.Add(It.IsAny<Customer>()));
         }
         
         [TestMethod]
         public void AddCommand_SelectedCustomer_ShouldNotBeNull()
         {
-            var repository = A.Fake<ICustomerRepository>();
-            var vm = new MainViewModel(repository);
+            var repositoryMock = new Mock<ICustomerRepository>();
+            repositoryMock.Setup(repository => repository.Add(It.IsAny<Customer>())).Returns(true);
+            var vm = new MainViewModel(repositoryMock.Object);
+            
             vm.AddCommand.Execute(null);
+            
             vm.SelectedCustomer.Should().NotBeNull();
         }
         
         [TestMethod]
         public void AddCommand_ShouldNotifyCustomers()
         {
-            var repository = A.Fake<ICustomerRepository>();
+            var repository = Mock.Of<ICustomerRepository>();
             var vm = new MainViewModel(repository);
             var wasNotified = false;
             vm.PropertyChanged += (_, e) =>
@@ -68,81 +80,98 @@ namespace CustomerApp.Tests
                 if (e.PropertyName == "Customers")
                     wasNotified = true;
             };
+
             vm.AddCommand.Execute(null);
+            
             wasNotified.Should().BeTrue();
         }
         
         [TestMethod]
         public void RemoveCommand_SelectedCustomerNull_ShouldNotRemoveInRepository()
         {
-            var repository = A.Fake<ICustomerRepository>();
-            var vm = new MainViewModel(repository);
+            var repositoryMock = new Mock<ICustomerRepository>();
+            repositoryMock.Setup(repository => repository.Remove(It.IsAny<Customer>()));
+            var vm = new MainViewModel(repositoryMock.Object);
+            
             vm.RemoveCommand.Execute(null);
-            A.CallTo(() => repository.Remove(A<Customer>._)).MustNotHaveHappened();
+            
+            repositoryMock.Verify(repository => repository.Remove(It.IsAny<Customer>()), Times.Never);
         }
         
         [TestMethod]
         public void RemoveCommand_SelectedCustomerNotNull_ShouldRemoveInRepository()
         {
-            var repository = A.Fake<ICustomerRepository>();
-            var vm = new MainViewModel(repository);
-            vm.SelectedCustomer = new Customer();
+            var repositoryMock = new Mock<ICustomerRepository>();
+            repositoryMock.Setup(repository => repository.Remove(It.IsAny<Customer>()));
+            var vm = new MainViewModel(repositoryMock.Object) {SelectedCustomer = new Customer()};
+
             vm.RemoveCommand.Execute(null);
-            A.CallTo(() => repository.Remove(A<Customer>._)).MustHaveHappened();
+            
+            repositoryMock.Verify(repository => repository.Remove(It.IsAny<Customer>()));
         }
         
         [TestMethod]
         public void RemoveCommand_SelectedCustomer_ShouldBeNull()
         {
-            var repository = A.Fake<ICustomerRepository>();
+            var repository = Mock.Of<ICustomerRepository>();
             var vm = new MainViewModel(repository);
             vm.SelectedCustomer = new Customer();
+            
             vm.RemoveCommand.Execute(null);
+
             vm.SelectedCustomer.Should().BeNull();
         }
         
         [TestMethod]
         public void RemoveCommand_ShouldNotifyCustomers()
         {
-            var repository = A.Fake<ICustomerRepository>();
-            var vm = new MainViewModel(repository);
-            vm.SelectedCustomer = new Customer(); 
+            var repository = Mock.Of<ICustomerRepository>();
+            var vm = new MainViewModel(repository) {SelectedCustomer = new Customer()};
             var wasNotified = false;
             vm.PropertyChanged += (_, e) =>
             {
                 if (e.PropertyName == "Customers")
                     wasNotified = true;
             };
+
             vm.RemoveCommand.Execute(null);
+            
             wasNotified.Should().BeTrue();
         }
         
         [TestMethod]
         public void SaveCommand_ShouldCommitInRepository()
         {
-            var repository = A.Fake<ICustomerRepository>();
-            var vm = new MainViewModel(repository);
+            var repositoryMock = new Mock<ICustomerRepository>();
+            repositoryMock.Setup(repository => repository.Commit());
+            var vm = new MainViewModel(repositoryMock.Object) {SelectedCustomer = new Customer()};
+
             vm.SaveCommand.Execute(null);
-            A.CallTo(() => repository.Commit()).MustHaveHappened();
+            
+            repositoryMock.Verify(repository => repository.Commit());
         }
         
         [TestMethod]
         public void SearchCommand_WithText_ShouldSetFilter()
         {
-            var repository = A.Fake<ICustomerRepository>();
+            var repository = Mock.Of<ICustomerRepository>();
             var vm = new MainViewModel(repository);
+
             vm.SearchCommand.Execute("text");
             var collection = CollectionViewSource.GetDefaultView(vm.Customers);
+            
             collection.Filter.Should().NotBeNull();
         }
         
         [TestMethod]
         public void SearchCommand_WithoutText_ShouldSetFilter()
         {
-            var repository = A.Fake<ICustomerRepository>();
+            var repository = Mock.Of<ICustomerRepository>();
             var vm = new MainViewModel(repository);
+
             vm.SearchCommand.Execute("");
             var collection = CollectionViewSource.GetDefaultView(vm.Customers);
+            
             collection.Filter.Should().BeNull();
         }
     }
